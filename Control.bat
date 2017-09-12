f4b0d344a63f8406210203df13606900414c8aa8
Timo <azzurite@gmail.com>
2017-09-12 13:40:46 +0200

    Extend control.bat to make it possible to define an IP

--- Control.bat
+++ Control.bat
@@ -1,2 +1,13 @@
-start Control.html
-start java -jar StreamViewer.jar
+set ip=
+set mask=
+
+if "%ip%" == "" goto :noip
+
+start "" "C:\Program Files\Firefox Developer Edition\firefox.exe" "Control.html?ip=%ip%"
+start "" java -jar StreamViewer.jar %ip% %mask%
+goto :eof
+
+:noip
+start "" explorer "Control.html"
+start "" java -jar StreamViewer.jar
+
